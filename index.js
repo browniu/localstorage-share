73bb60989fc3692315b910735a826ac7f4b15f23
browniu <niuniusoga@qq.com>
2019-10-12 11:04:11 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,32 @@
+
+Penpal.connectToParent({
+    methods: {
+        get: (key) => get(key),
+        set: (key, value) => set(key, value),
+        clear: () => clear(),
+        getAll: () => getAll()
+    }
+});
+
+function get(key) {
+    const SSC = JSON.parse(localStorage.getItem('storageShareCenter')) || {};
+    const value = SSC[key];
+    return {[key]: value};
+}
+
+function set(key, value) {
+    const SSC = JSON.parse(localStorage.getItem('storageShareCenter')) || {};
+    SSC[key] = value;
+    localStorage.setItem('storageShareCenter', JSON.stringify(SSC));
+    return {[key]: value}
+}
+
+function clear() {
+    const SSC = JSON.parse(localStorage.getItem('storageShareCenter')) || {};
+    localStorage.setItem('storageShareCenter', '');
+    return SSC
+}
+
+function getAll() {
+    return JSON.parse(localStorage.getItem('storageShareCenter')) || {};
+}
